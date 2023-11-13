5c639beb599632762b695a8b9775dd8e70dee86d
hclaude <hippolyteclaude@icloud.com>
2023-11-13 16:58:01 +0100

    sdfghj

--- printf.h
+++ printf.h
@@ -6,7 +6,7 @@
 /*   By: hclaude <hclaude@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 13:15:28 by hclaude           #+#    #+#             */
-/*   Updated: 2023/11/10 13:17:05 by hclaude          ###   ########.fr       */
+/*   Updated: 2023/11/13 16:51:45 by hclaude          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdarg.h>
+# include "libft.h"
 
 
 #endif
--- printf_format.c
+++ printf_format.c
@@ -1,35 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   printf_format.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hclaude <hclaude@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/10 13:08:23 by hclaude           #+#    #+#             */
-/*   Updated: 2023/11/13 16:44:39 by hclaude          ###   ########.fr       */
+/*   Created: 2023/11/13 16:51:06 by hclaude           #+#    #+#             */
+/*   Updated: 2023/11/13 16:52:32 by hclaude          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
-
-int ft_printf(const char *format, ...)
-{
-	va_list ap;
-	int i;
-	int count;
-	
-	va_start(ap, format);
-	i = 0;
-	count = 0;
-	while (format[i])
-	{
-		if (format[i] == '%')
-			count += printf_format(format[i], ap);
-		else
-			count += write(1, format[i], 1);
-		i++;
-	}
-	va_end(ap);
-	return (count);
-}
 
